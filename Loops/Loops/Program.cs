ad970086fb475a622422fb529b7f5de886f25b81
ZShick <zshick@pdx.edu>
2021-07-18 16:58:24 -0700

    Step 229 polished

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -10,10 +10,14 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            //telling the user to guess a color from 4 options.
             Console.WriteLine("Guess the correct color. Options are blue, red, yellow, and green.");
             string color = Console.ReadLine();
 
+            //the correct answer is red. isGeussed is true when red is entered
             bool isGuessed = color == "red";
+
+            //do statement. The following are instructions for the program to follow until isGuessed is true
             do
             {
                 switch (color)
@@ -33,10 +37,14 @@ namespace Loops
                         Console.WriteLine("Guess a color. Options are blue, red, yellow, and green.");
                         color = Console.ReadLine();
                         break;
+
+                        //the correct answer is here. It causes isGuessed to = true which stops the do loop from continuing.
                     case "red":
                         Console.WriteLine("You guessed red. That is correct!");
                         isGuessed = true;
                         break;
+
+                        //the default cathes any other entries or mispellings.
                     default:
                         Console.WriteLine("Incorrect. That is not one of the options to choose from.");
                         Console.WriteLine("Guess a color. Options are blue, red, yellow, and green.");
@@ -45,6 +53,7 @@ namespace Loops
                 }
 
             }
+            //the while statement. The above chunk of code repeats until red is guessed.
             while (!isGuessed);
 
             Console.ReadLine();
