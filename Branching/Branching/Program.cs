adbb86506e6c377307b25b8e9f9eccafbdae96a6
ZShick <zshick@pdx.edu>
2021-07-17 21:28:52 -0700

    Step 222 Assignment

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have a great favorite number!" : "Your number sucks.";
+
+            Console.WriteLine(result);
+
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhhhh.... something went wrong.");
+            //}
+
+            //int currentTemperature = 60;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is exactly room temperature." : "It is not exactly room temperature.";
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than it is right now");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+            //Console.WriteLine(comparisonResult);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ step222/step222/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //welcomes user and gets the weight of the package
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\nHow much does your package weigh?");
+            float weight = Convert.ToSingle(Console.ReadLine());
+
+            //checks to see if the wieght is more than 50 or not. If it is, the user is informed that it is too heavy to be shipped. If not, then it moves on and asks for the width.
+            string isItTooHeavy = weight > 50 ? "Package too heavy to be shipped via Package Express. Have a good day." : "What is the width of your package?";
+            Console.WriteLine(isItTooHeavy);
+            float width = Convert.ToSingle(Console.ReadLine());
+
+            //asks for the height
+            Console.WriteLine("What is the height of your package?");
+            float height = Convert.ToSingle(Console.ReadLine());
+
+            //asks for the length
+            Console.WriteLine("What is the length of your package?");
+            float length = Convert.ToSingle(Console.ReadLine());
+
+            //adds up the 3 dimensions given to see if the total is greater than 50. If it is, the user is informed that it is too big to be shipped. If not, then it lists the final price.
+            string isItTooBig = (width + height + length) > 50 ? "Package too big to be shipped via Package Express." : "Your estimated total for shipping this package is:";
+            Console.WriteLine(isItTooBig);
+
+            //final price is determined by multiplying all dimensions together. Then the product of that is multiplied by the weight. Finally, that product is divided by 100. The quotient is the final quote.
+            float total = ((width * height * length) * weight) / 100;
+            Console.WriteLine("$" + total + "\nThank you!");
+            Console.ReadLine();
+
+        }
+    }
+}
