8d42e38f5f39d8b6015f30b9622245c90414bbc6
ZShick <zshick@pdx.edu>
2021-07-23 08:03:26 -0700

    Step280 Assignment

---
+++ Step280/Step280/optionalPs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step280
+{
+    public class OptionalPs
+    {
+        public int opParam(int num, int num1 = 0)
+        {
+            //just a simple addition method
+            return num + num1;
+        }
+    }
+}
