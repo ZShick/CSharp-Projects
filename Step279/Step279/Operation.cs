19d12ab84363e114c1592ff1e3471e5e04d37f68
ZShick <zshick@pdx.edu>
2021-07-23 07:12:34 -0700

    Step279 Assignment

---
+++ Step279/Step279/Operation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step279
+{
+    public class Operation
+    {
+        //adds 5 to the whatever the number is and returns it
+        public int Add(int num)
+        {
+            return num + 5;
+        }
+        //adds 2 to whatever the decimal is and returns it
+        public decimal Add(decimal num1)
+        {
+            return num1 + 2;
+        }
+        //converts the string into an integer, then adds 3 to it before returning it
+        public int Add(string num2)
+        {
+            int num3 = Convert.ToInt32(num2);
+            return num3 + 3;
+        }
+    }
+}
