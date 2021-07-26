4ea5d0abc37a81ccfbf70d8ae2aaef5ace569046
ZShick <zshick@pdx.edu>
2021-07-26 03:40:59 -0700

    Step383

---
+++ Step383/Step383/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Step383
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //created a constant and then created an int variable using the "var" keyword
+            const int y = 10;
+            var x = 10;
+        }
+    }
+}
